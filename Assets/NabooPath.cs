06bd8d2a6a96ec91358a3f6d160f45a0ae413635
James Gormley <james.gormley@mydit.ie>
2016-02-17 20:24:06 +0000

    Project Files
    Not good, but something

---
+++ Assets/NabooPath.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NabooPath : MonoBehaviour {
+    Boid boid;
+    public float radius = 50;
+    // Use this for initialization
+    void Start () {
+        boid = GetComponent<Boid>();
+        boid.seekEnabled = true;
+        PickNewTarget();
+    }
+
+    void PickNewTarget()
+    {
+        Vector3 target = Random.insideUnitSphere;
+        target.y = 0;
+        target *= radius;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Vector3.Distance(transform.position, boid.seekTargetPosition) < 1.0f)
+        {
+            PickNewTarget();
+        }
+
+    }
+
+
+}
